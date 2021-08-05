2e0202973f0ad8a75d80a97eb31d285a6213593a
Linh Hoang <Linh.Hoang@devmounta.in>
2021-08-05 13:10:14 -0500

    final

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const accountRoutes = require("./routes/accounts");
+const servicesRoutes = require("./routes/services");
+
+const { accounts, users, writeJSON } = require("./data");
 
 const app = express();
 
@@ -8,15 +12,18 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
-
-const accountData = fs.readFileSync("src/json/accounts.json", "UTF8");
-const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync("src/json/users.json", "UTF8");
-const users = JSON.parse(userData);
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.render("index", { title: "Account Summary", accounts: accounts });
 });
+
+app.get("/profile", (req, res) => {
+	res.render("profile", { user: users[0] });
+});
+
+app.use("/account", accountRoutes);
+app.use("/services", servicesRoutes);
 
 app.listen(3000, () => {
 	console.log("PS Project Running on port 3000!");

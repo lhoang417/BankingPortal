8d60066bda3f33ea27c31c8c588f67ba0c4cfe5b
Linh Hoang <Linh.Hoang@devmounta.in>
2021-08-05 13:13:15 -0500

    final

---
+++ src/routes/services.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+const { accounts, writeJSON } = require("../data");
+
+router.post("/transfer", (req, res) => {
+	accounts[req.body.from].balance -= req.body.amount;
+	accounts[req.body.to].balance += parseInt(req.body.amount);
+	writeJSON();
+	res.render("transfer", { message: "Transfer Completed" });
+});
+
+router.post("/payment", (req, res) => {
+	accounts.credit.balance -= req.body.amount;
+	accounts.credit.available += parseInt(req.body.amount);
+	writeJSON();
+	res.render("payment", {
+		message: "Payment Successful",
+		account: accounts.credit,
+	});
+});
+router.get("/transfer", (req, res) => {
+	res.render("transfer");
+});
+
+router.get("/payment", (req, res) => {
+	res.render("payment", { accounts: accounts.credit });
+});
+
+module.exports = router;
